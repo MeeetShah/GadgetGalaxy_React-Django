3cb8cc7983d941d3ace2d24a582b1d32cfcf09e5
MeeetShah <meet1110shah@gmail.com>
2024-09-16 23:43:21 +0530

    Navbar css added

--- gadgets/src/Components/Navbar.js
+++ gadgets/src/Components/Navbar.js
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import "../Navbar.css";
 
-
 const Navbar = () => {
   const names = useSelector((state) => state.products);
   const navigate = useNavigate();
@@ -16,109 +15,109 @@ const Navbar = () => {
 
   return (
     <>
-   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          <i className="fas fa-plane-departure"></i> Navbar
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
-                <i className="fas fa-home"></i> Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                <i className="fas fa-info-circle"></i> About Us
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                <i className="fas fa-envelope"></i> Contact Us
-              </Link>
-            </li>
-            <li className="nav-item dropdown">
-              <Link
-                className="nav-link dropdown-toggle"
-                to="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-                onClick={() => setShowDropdown(!showDropdown)}
-              >
-                <i className="fas fa-box"></i> Products
-              </Link>
-              <ul className={`dropdown-menu${showDropdown ? ' show' : ''}`}>
-                {names?.length > 0 &&
-                  names.map((name) => (
-                    <li
-                      key={name.id}
-                      className="dropdown-item"
-                      style={{ cursor: "pointer" }}
-                      onClick={() => gotoitem(name.id)}
-                    >
-                      {name.name}
-                    </li>
-                  ))}
-              </ul>
-            </li>
-            <form className="d-flex ms-3" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                <i className="fas fa-search"></i> Search
-              </button>
-            </form>
-          </ul>
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            {!login && (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+          <i class="fa-brands fa-gg-circle"></i> GadgetGalaxy
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link" to="/login">
-                  <i className="fas fa-sign-in-alt"></i> Login
+                <Link className="nav-link active" aria-current="page" to="/">
+                  <i className="fas fa-home"></i> Home
                 </Link>
               </li>
-            )}
-            {!login && (
               <li className="nav-item">
-                <Link className="nav-link" to="/signup">
-                  <i className="fas fa-user-plus"></i> Signup
+                <Link className="nav-link" to="/about">
+                  <i className="fas fa-info-circle"></i> About Us
                 </Link>
               </li>
-            )}
-            {login && (
               <li className="nav-item">
-                <Link className="nav-link" to="/logout">
-                  <i className="fas fa-sign-out-alt"></i> Logout
+                <Link className="nav-link" to="/contact">
+                  <i className="fas fa-envelope"></i> Contact Us
                 </Link>
               </li>
-            )}
-            {login && (
-              <li className="nav-item">
-                <Link className="nav-link" to="/cart">
-                  <i className="fas fa-shopping-cart"></i> Cart
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  onClick={() => setShowDropdown(!showDropdown)}
+                >
+                  <i className="fas fa-box"></i> Products
                 </Link>
+                <ul className={`dropdown-menu${showDropdown ? " show" : ""}`}>
+                  {names?.length > 0 &&
+                    names.map((name) => (
+                      <li
+                        key={name.id}
+                        className="dropdown-item"
+                        style={{ cursor: "pointer" }}
+                        onClick={() => gotoitem(name.id)}
+                      >
+                        {name.name}
+                      </li>
+                    ))}
+                </ul>
               </li>
-            )}
-          </ul>
+              <form className="d-flex ms-3" role="search">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button className="btn btn-outline-success" type="submit">
+                  <i className="fas fa-search"></i> Search
+                </button>
+              </form>
+            </ul>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              {!login && (
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">
+                    <i className="fas fa-sign-in-alt"></i> Login
+                  </Link>
+                </li>
+              )}
+              {!login && (
+                <li className="nav-item">
+                  <Link className="nav-link" to="/signup">
+                    <i className="fas fa-user-plus"></i> Signup
+                  </Link>
+                </li>
+              )}
+              {login && (
+                <li className="nav-item">
+                  <Link className="nav-link" to="/logout">
+                    <i className="fas fa-sign-out-alt"></i> Logout
+                  </Link>
+                </li>
+              )}
+              {login && (
+                <li className="nav-item">
+                  <Link className="nav-link" to="/cart">
+                    <i className="fas fa-shopping-cart"></i> Cart
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     </>
   );
 };
